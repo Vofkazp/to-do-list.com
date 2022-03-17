bd4105e869e6bbe2d97f23a4cc62e94dc68804be
Vofkazp <Vofkazp@gmail.com>
2022-03-17 19:05:10 +0200

    fix

--- components/Db.php
+++ components/Db.php
@@ -5,7 +5,7 @@ class Db {
     public static function getConnection() {
         $paramsPath = ROOT . '/components/db_params.php';
         $params = include($paramsPath);
-        $dsn = "mysql:host={$params['host']};dbname={$params['dbname']}";
+        $dsn = "mysql:host={$params['host']};port={$params['port']};dbname={$params['dbname']}";
         $db = new PDO($dsn, $params['user'], $params['password']);
         $db->exec("set names utf8");
         return $db;
--- components/db_params.php
+++ components/db_params.php
@@ -2,6 +2,7 @@
 return array(
     'host' => 'ec2-54-195-246-55.eu-west-1.compute.amazonaws.com',
     'dbname' => 'de9up7q5mq9cbr',
+    'port' => '5432',
     'user' => 'mqxwlaiilyejxf',
     'password' => '613e7dd2d2c739f6651773733277c5699292c7280ea4fa88e164ae4f98d1d424',
 );
